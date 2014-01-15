9f8dee46aabfb8562ac60ef0aeb98ac7af670d36
Jon Nordby <jononor@gmail.com>
2014-01-15 02:32:27 +0100

    Factorize components

--- pyflo.py
+++ pyflo.py
@@ -1,6 +1,7 @@
 import sys, os
+import functools
 
-sys.setrecursionlimit(10)
+sys.setrecursionlimit(100)
 
 class Port(object):
     def __init__(self):
@@ -22,13 +23,17 @@ class Component(object):
             pass
 
 class Unary(Component):
+    @staticmethod
+    def factory(func):
+        return functools.partial(Unary, func)
+
     def __init__(self, function):
         Component.__init__(self)
+        self._func = function
         self.ports = {
             "in": Port(),
             "out": Port()
         }
-        self._func = function
 
     def receive(self, data, port):
         self.send(self._func(data), "out")
@@ -39,19 +44,32 @@ def connect(a, srcport, tgtport, b):
 def send(n, data):
     n.receive(data, "in")
 
-# TODO: make these factories and not instances
+Invert = Unary.factory(lambda obj: not obj)
+IncrementOne = Unary.factory(lambda obj: obj+1)
+WriteStdOut = Unary.factory(lambda obj: sys.stdout.write(obj))
+Str = Unary.factory(lambda obj: str(obj))
 
-invert = Unary(lambda obj: not obj)
-add1 = Unary(lambda obj: obj+1)
-stdout = Unary(lambda obj: sys.stdout.write(obj) )
-stringify = Unary(lambda obj: str(obj))
+# Test program
+invert = Invert()
+invert2 = Invert()
+stringify = Str()
+stdout = WriteStdOut()
 
-
-connect(invert, "out", "in", stringify)
+connect(invert, "out", "in", invert2)
+connect(invert2, "out", "in", stringify)
 connect(stringify, "out", "in", stdout)
 
 send(stdout, str(True))
 send(stdout, "\n")
+send(invert2, True)
+send(stdout, "\n")
 send(invert, True)
 send(stdout, "\n")
 
+# s
+out = WriteStdOut()
+inc = IncrementOne()
+s = Str()
+connect(inc, "out", "in", s)
+connect(s, "out", "in", out)
+send(inc, 1)
