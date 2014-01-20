5eae2b84a38b97c7a683977619cebe467651ad47
Jon Nordby <jononor@gmail.com>
2014-01-20 21:45:41 +0100

    Parse graph from .json, introduce Network API

--- pyflo.py
+++ pyflo.py
@@ -1,5 +1,7 @@
 import sys, os
 import functools
+import json
+import subprocess
 
 sys.setrecursionlimit(100)
 
@@ -38,38 +40,95 @@ class Unary(Component):
     def receive(self, data, port):
         self.send(self._func(data), "out")
 
-def connect(a, srcport, tgtport, b):
-    a.ports[srcport].target = (b, tgtport)
+components = {
+    "Invert": Unary.factory(lambda obj: not obj),
+    "IncrementOne": Unary.factory(lambda obj: obj+1),
+    "WriteStdOut": Unary.factory(lambda obj: sys.stdout.write(obj)),
+    "Str": Unary.factory(lambda obj: str(obj)),
+}
 
-def send(n, data):
-    n.receive(data, "in")
 
-Invert = Unary.factory(lambda obj: not obj)
-IncrementOne = Unary.factory(lambda obj: obj+1)
-WriteStdOut = Unary.factory(lambda obj: sys.stdout.write(obj))
-Str = Unary.factory(lambda obj: str(obj))
+class Network(object):
+    def __init__(self, graph):
+        self._graph = graph
+        self.stop()
+
+    def stop(self):
+        self._state = "stopped"
+        self._nodes = {}
+        self._msgqueue = []
+
+    def start(self):
+        # Instantiate components
+        graph = self._graph
+        for name, data in graph['processes'].items():
+            self._nodes[name] = components[data['component']]()
+
+        # Wire up ports, IIPs
+        for conn in graph['connections']:
+            tgt = conn['tgt']
+            src = conn.get('src', None)
+            data = conn.get('data', None)
+            if src:
+                self.connect(src['process'], src['port'],
+                             tgt['process'], tgt['port'])
+            elif data is not None:
+                iip = (tgt['process'], tgt['port'], data)
+                self.send(*iip)
+            else:
+                raise ValueError, "No src node or IIP"
+       
+
+    def connect(self, src, srcport, tgt, tgtport):
+        if not isinstance(src, Component):
+            src = self._nodes[src]
+        if not isinstance(tgt, Component):
+            tgt = self._nodes[tgt]
+
+        src.ports[srcport].target = (tgt, tgtport)
+     
+    def send(self, tgt, port, data):
+        if not isinstance(tgt, Component):
+            tgt = self._nodes[tgt]        
+
+        ip = (tgt, port, data)
+        self._msgqueue.append(ip)
+
+    def _deliver_messages(self):
+        stop_index = len(self._msgqueue)
+        for index, msg in enumerate(self._msgqueue[0:stop_index]):
+            if index == stop_index:
+                break
+            tgt, port, data = msg
+            tgt.receive(data, port)
+        self._msgqueue = self._msgqueue[stop_index:]
+
+    def run_iteration(self):
+        self._deliver_messages()
+
+        
+
+def load_file(path):
+    if not path.endswith(".json"):
+        raise ValueError, "Only JSON FBP format supported"
+
+    f = open(path, "r")
+    return json.loads(f.read())
+
 
 # Test program
-invert = Invert()
-invert2 = Invert()
-stringify = Str()
-stdout = WriteStdOut()
+def test1():
+    out = WriteStdOut()
+    inc = IncrementOne()
+    s = Str()
+    connect(inc, "out", "in", s)
+    connect(s, "out", "in", out)
+    send(inc, 1)
 
-connect(invert, "out", "in", invert2)
-connect(invert2, "out", "in", stringify)
-connect(stringify, "out", "in", stdout)
+def test2():
+    net = Network(load_file("examples/first.json"))
+    net.start()
+    net.run_iteration()
 
-send(stdout, str(True))
-send(stdout, "\n")
-send(invert2, True)
-send(stdout, "\n")
-send(invert, True)
-send(stdout, "\n")
-
-# s
-out = WriteStdOut()
-inc = IncrementOne()
-s = Str()
-connect(inc, "out", "in", s)
-connect(s, "out", "in", out)
-send(inc, 1)
+if __name__ == "__main__":
+    test2()
